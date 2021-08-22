00e1715697357cc9dd61f9a087ba550aba07f40b
rkdeowk <30549314+rkdeowk@users.noreply.github.com>
2021-08-22 19:22:35 +0900

    chatApp

---
+++ SuperChat/SuperChat/MVVM/Model/ContactModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace SuperChat.MVVM.Model
+{
+    class ContactModel
+    {
+        public string Username { get; set; }
+        public string ImageSource { get; set; }
+        public ObservableCollection<MessageModel> Messages { get; set; }
+        public string LastMessage => Messages.Last().Message;
+    }
+}
---
+++ SuperChat/SuperChat/MVVM/Model/MessageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SuperChat.MVVM.Model
+{
+    class MessageModel
+    {
+        public string Username { get; set; }
+        public string UsernameColor { get; set; }
+        public string ImageSource { get; set; }
+        public string Message { get; set; }
+        public DateTime Time { get; set; }
+        public bool IsNativeOrigin { get; set; }
+        public bool FirstMessage { get; set; }
+    }
+}
